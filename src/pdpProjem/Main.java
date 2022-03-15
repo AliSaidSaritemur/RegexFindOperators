a0895b9a91267470088537ee8a4a1b6634a8ded9
AliSaidSaritemur <80647702+AliSaidSaritemur@users.noreply.github.com>
2022-03-15 23:42:58 +0300

    Dosyalar Duzenlendi

--- src/pdpProjem/Main.java
+++ src/pdpProjem/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException{
 
-		File file =new File("Lexical.java");
+		File file =new File("dist/Lexical.java");
 		
 		if(! file.exists()) {
 			
